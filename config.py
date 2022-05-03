9e73163033c5b547cee93e27260afbe160164a1c
Robert <rgitonga.07@gmail.com>
2022-05-03 23:39:11 +0300

    add configuration

--- config.py
+++ config.py
@@ -17,3 +17,19 @@ class Config:
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
