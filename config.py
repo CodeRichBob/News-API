51263fdfdb94124d6366ad631c8a7886b9884bc1
Robert <rgitonga.07@gmail.com>
2022-05-03 23:36:53 +0300

    add virtual env

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+    class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
